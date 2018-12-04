bd3b98dca2802b12c192cf3c3e5f99641eb7dd8c
lsyefficient <1024847801@qq.com>
2018-12-04 11:55:53 +0800

    first commit

---
+++ widget.cpp
@@ -0,0 +1,41 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    setFixedSize(width(),height());
+
+    auto saveImage=[&]{
+        if(!m_GifWriter)return;
+        auto image = grab().toImage().convertToFormat(QImage::Format_RGBA8888);
+        m_Gif.GifWriteFrame(m_GifWriter, image.bits(), image.width(), image.height(), m_Fps);
+    };
+    connect(&timer,&QTimer::timeout,saveImage);
+
+    connect(ui->pushButton,&QPushButton::clicked,[&](bool checked){
+        if(checked){
+            auto file = QTime::currentTime().toString("hh-mm-ss.gif");
+            if(m_GifWriter)delete m_GifWriter;
+            m_GifWriter = new Gif_H::GifWriter;
+            if(!m_Gif.GifBegin(m_GifWriter,file.toLocal8Bit().data(),width(),height(), m_Fps)){
+                delete m_GifWriter;
+                m_GifWriter = nullptr;
+                return;
+            }
+            saveImage();
+            timer.start(m_Fps*10);
+        }else{
+            timer.stop();
+            m_Gif.GifEnd(m_GifWriter);
+            delete m_GifWriter;
+            m_GifWriter = nullptr;
+        }
+    });
+}
+
+Widget::~Widget(){
+    delete ui;
+}
---
+++ widget.h
@@ -0,0 +1,27 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QtWidgets>
+#include <gif.h>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+    QTimer timer;
+    //
+    Gif_H m_Gif;
+    Gif_H::GifWriter *m_GifWriter = nullptr;
+    uint  m_Fps = 33;//100为1s
+};
+
+#endif // WIDGET_H
